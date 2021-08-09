d11f10ef6bb5eb199a69264ff0d614619af5c212
yjacks <67487960+yjacks@users.noreply.github.com>
2021-08-09 23:41:43 +0800

    DIYLiveCD大体完成

--- correctLiveCD.py
+++ correctLiveCD.py
@@ -1,1 +1,54 @@
 # 还没写
+
+from globalProject import *
+
+iso_name = ""
+
+
+def liveBuild():
+    with open("a.sh", "w") as w:
+        w.write("cd " + configDir + "/live\n")
+        w.write("lb build")
+    os.system("sh a.sh")
+    a = os.popen("ls " + configDir + "/live *.iso")
+    i = a.read()
+    a.close()
+    global iso_name
+    iso_name = i[i.rfind("/") + 1:-1]
+
+
+def mountIso():
+    os.mkdir("iso_cp")
+    os.mkdir("iso")
+    os.system("mount -o loop " + iso_name + " iso_cp")
+    os.system("cp -av iso_cp iso")
+    os.system("cp -av iso_cp/.disk iso")
+    os.system("umount iso_cp")
+
+
+def DIY_live():
+    os.mkdir("squashfs-root")
+    os.system("unsquashfs iso/live/filesystem.squashfs squashfs-root")
+    os.system("mount --bind /dev ~/squashfs-root/dev")
+    os.system("mount -t proc proc ~/squashfs-root/proc")
+    os.system("mount -t sysfs sys ~/squashfs-root/sys")
+    os.system("cp /etc/resolv.conf ~/squashfs-root/etc/")
+    os.system("cp /etc/apt/source.list ~/squashfs-root/etc/apt/")
+    clearWindow()
+    print("开始自定义")
+    os.system("chroot squashfs-root")
+
+    # 退出后
+
+    os.system("umount squashfs-root/dev")
+    os.system("umount squashfs-root/proc")
+    os.system("umount squashfs-root/sys")
+    os.system("mkdir squashfs-root/dev")
+    os.system("mkdir squashfs-root/proc")
+    os.system("mkdir squashfs-root/sys")
+    os.system("rm iso/live/filesystem.squashfs")
+    os.system("mksquashfs squashfs-root iso/live/filesystem.squashfs")
+
+
+def isoBuild():
+    pass
--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 # 测试
 import config
+import correctLiveCD
 
 config.buildConfig()
 config.readConfig()
 config.configSend()
 config.writeConfig()
+correctLiveCD.liveBuild()
+correctLiveCD.mountIso()
+correctLiveCD.DIY_live()
